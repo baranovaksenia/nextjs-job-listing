bcfaa542e1e53afd08dd9d10dd6cc8d8a08227c4
Ksenia <baranova-ksenia1990@gmail.com>
2024-02-19 17:47:29 +0200

    add utility functions, fetch jobs, create joblistitem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline flex mx-auto">hello</h1>;
+import JobListItem from "@/components/JobListItem";
+import prisma from "@/lib/prisma";
+export default async function Home() {
+  // fetch all available jobs from db
+  const jobs = await prisma.job.findMany({
+    // show the newest on the top
+    where: {
+      approved: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return (
+    <main>
+      {jobs.map((job) => (
+        <JobListItem job={job} key={job.id} />
+      ))}
+    </main>
+  );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,33 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { formatDistanceToNowStrict } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines multiple class names into a single string
+ * @param inputs - The class names to combine
+ * @returns The combined class names as a string
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * Formats the given amount as a currency in en-US locale
+ * @param amount - The amount to format as currency
+ * @returns The formatted currency string
+ */
+export function formatMoney(amount: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
+}
+
+/**
+ * Returns the relative date from the given date
+ * @param from - The date to calculate the relative date from
+ * @returns The relative date as a string
+ */
+export function relativeDate(from: Date) {
+  return formatDistanceToNowStrict(from, { addSuffix: true });
 }
